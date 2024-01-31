cb982fb1bfe5c935a309f42b6e95dc03e26fc59c
igorfcfs <igorfcfs@gmail.com>
2024-01-31 11:00:23 -0300

    Classe e Método no C#

---
+++ Musica.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound
+{
+    internal class Musica
+    {
+        public string nome;
+        public string artista;
+        public int duracao;
+        public bool disponivel;
+
+        public void ExibirFichaTecnica()
+        {
+            Console.WriteLine($"Nome da música: {nome}");
+            Console.WriteLine($"Nome do artista: {artista}");
+            Console.WriteLine($"Duração: {duracao}");
+
+            if (disponivel)
+            {
+                Console.WriteLine("Disponível no plano");
+            }
+            else
+            {
+                Console.WriteLine("Adquira o plano Plus+");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,26 @@ namespace ScreenSound
     {
         static void Main(string[] args)
         {
+            Musica musica1 = new Musica();
+            musica1.nome = "Roxane";
+            musica1.artista = "The Police";
+            musica1.duracao = 273;
+            musica1.disponivel = true;
+
+            //Console.WriteLine($"Nome da música {musica1.nome}");
+            //Console.WriteLine($"Nome da música {musica1.artista}");
+
+            musica1.ExibirFichaTecnica();
+
+            Musica musica2 = new Musica();
+            musica2.nome = "Vertigo";
+            musica2.artista = "U2";
+            musica1.duracao = 367;
+            musica1.disponivel = false;
+
+            //Console.WriteLine($"Nome da música {musica2.nome}");
+            //Console.WriteLine($"Nome da música {musica2.artista}");
+            musica2.ExibirFichaTecnica();
         }
     }
 }
