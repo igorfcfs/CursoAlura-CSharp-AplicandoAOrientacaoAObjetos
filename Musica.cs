74bae7eda13dc7d0a17fd08d25b37b6c3f7f3476
igorfcfs <igorfcfs@gmail.com>
2024-01-31 11:28:17 -0300

    Métodos de Acesso e Propriedades

--- Musica.cs
+++ Musica.cs
@@ -8,18 +8,34 @@ namespace ScreenSound
 {
     internal class Musica
     {
-        public string nome;
-        public string artista;
-        public int duracao;
-        public bool disponivel;
+        /*Atalhos:
+        Ctrl + R + R -> renomeia uma propriedade
+        cw + tab -> Console.WriteLine();
+        prop + TAB -> public int MyProperty { get; set; }
+         */
+        public string Nome { get; set; }
+        public string Artista { get; set; }
+        public int Duracao { get; set; }
+        public bool Disponivel { get; set; }
+
+        //Propriedade somente leitura sem lambda
+        /* public string DescricaoResumida {
+            get
+            {
+                return $"A música {Nome} pertence à banda {Artista}";
+            }
+        }*/
+
+        //Propriedade somente leitura com lambda
+        public string DescricaoResumida => $"A música {Nome} pertence à banda {Artista}";
 
         public void ExibirFichaTecnica()
         {
-            Console.WriteLine($"Nome da música: {nome}");
-            Console.WriteLine($"Nome do artista: {artista}");
-            Console.WriteLine($"Duração: {duracao}");
+            Console.WriteLine($"Nome da música: {Nome}");
+            Console.WriteLine($"Nome do artista: {Artista}");
+            Console.WriteLine($"Duração: {Duracao}");
 
-            if (disponivel)
+            if (Disponivel)
             {
                 Console.WriteLine("Disponível no plano");
             }
--- Program.cs
+++ Program.cs
@@ -11,25 +11,22 @@ namespace ScreenSound
         static void Main(string[] args)
         {
             Musica musica1 = new Musica();
-            musica1.nome = "Roxane";
-            musica1.artista = "The Police";
-            musica1.duracao = 273;
-            musica1.disponivel = true;
-
-            //Console.WriteLine($"Nome da música {musica1.nome}");
-            //Console.WriteLine($"Nome da música {musica1.artista}");
+            musica1.Nome = "Roxane";
+            musica1.Artista = "The Police";
+            musica1.Duracao = 273;
+            musica1.Disponivel = true;
 
             musica1.ExibirFichaTecnica();
+            Console.WriteLine(musica1.DescricaoResumida);
 
             Musica musica2 = new Musica();
-            musica2.nome = "Vertigo";
-            musica2.artista = "U2";
-            musica1.duracao = 367;
-            musica1.disponivel = false;
+            musica2.Nome = "Vertigo";
+            musica2.Artista = "U2";
+            musica1.Duracao = 367;
+            musica1.Disponivel = false;
 
-            //Console.WriteLine($"Nome da música {musica2.nome}");
-            //Console.WriteLine($"Nome da música {musica2.artista}");
             musica2.ExibirFichaTecnica();
+            Console.WriteLine(musica2.DescricaoResumida);
         }
     }
 }
